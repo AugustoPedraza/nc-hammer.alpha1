dbfdfd00a8c51bb02d1dc2875396b123041fc375
Augusto <augusto_pedraza@hotmail.com>
2012-11-19 20:55:15 -0300

    [Initial Commit]
     - Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NcHammerAlpha1::Application.config.secret_token = '0785957bfefc4742d5b64dbf2286d9c1189c1c523dbad44af966cc8c40829d05f84e5d096464f65af3a6f4375b86008470062fd3ad44afe8a6ca0c3249e9d002'
