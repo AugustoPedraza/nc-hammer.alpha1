53bd80067e8a2df27bcfdb26348d41df7cf96716
Augusto <augusto_pedraza@hotmail.com>
2012-11-20 22:30:51 -0300

    [Associate company to new user]
     - Added method to find or create one company.
     - Associate company to new user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   def after_sign_in_path_for(resource)
+    current_user.companies << Company.find_or_create(params[:user][:company_name])
+    puts "="*50
+    puts current_user.companies.inspect
+    puts "="*50
     '/'
   end
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,8 @@ class Company < ActiveRecord::Base
   attr_accessible :name
 
   has_and_belongs_to_many :users
+
+  def self.find_or_create(name)    
+    where('name = ?', name).first || create!(name: name)
+  end
 end
